d8741bce5a34fb8355537028afa8f80e4f9ba981
Joseph Torii <jtorii@zagmail.gonzaga.edu>
2020-05-01 10:33:26 -0700

    deplyment

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -34,10 +34,10 @@ app.use('/users', usersRouter);
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     // set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('my-app/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'));
     });
 }
 
